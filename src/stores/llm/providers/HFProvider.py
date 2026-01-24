e40bbbc67953a4f353208e0f07b21b461d9d0728
SaraaElsayed <saraeibrahim9@gmail.com>
2026-01-24 18:14:28 +0200

    switch to postgres

--- src/stores/llm/providers/HFProvider.py
+++ src/stores/llm/providers/HFProvider.py
@@ -100,5 +100,5 @@ class HuggingFaceProvider(LLMInterface):
     def construct_prompt(self, prompt: str, role: str):
         return {
             "role": role,
-            "content": self.process_text(prompt)
+            "content": prompt
         }
